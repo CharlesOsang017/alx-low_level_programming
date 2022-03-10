c46cb2ce7eb9e618b5771c99410366208a27d137
CharlesOsang017 <charlesosango02@gmail.com>
2022-03-10 12:43:28 -0800

    Add quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,5 @@
+
+  
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,19 +9,7 @@
  */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-				\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
-
-
-
-
-
-
-{
-
-
-
-
