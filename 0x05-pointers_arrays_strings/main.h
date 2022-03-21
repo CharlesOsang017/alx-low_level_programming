f381de2893bc3e5aac46d4bc5ef89a84fdf206f8
CharlesOsang017 <charlesosango02@gmail.com>
2022-03-21 11:31:58 +0300

    Add puts.c file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 
+#ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
@@ -6,4 +8,5 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+#endif
 
