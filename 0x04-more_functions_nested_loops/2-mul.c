fd52ae9ce6eb38389f56ddc3540829895d7a2bac
CharlesOsang017 <charlesosango02@gmail.com>
2022-03-21 11:41:21 +0300

    'Hot fix'

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -11,3 +11,14 @@ int mul(int a, int b)
 {
 return (a * b);
 }
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+
+
+#endif
