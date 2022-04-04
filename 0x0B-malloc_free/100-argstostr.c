64bdcb97c5071529f10f40b8e7ebd92dfa32b282
CharlesOsang017 <charlesosango02@gmail.com>
2022-04-04 20:38:02 +0300

    Hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,79 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-char **spll(char *str, int cont1);
+
 /**
-  * strtow - splits a string into words
-  * @str: a string with spaces
+  * argstostr - fills memory with a constant byte
+  * @ac: is the size of the pointer
+  * @av: is a letter
   * Return: an address
   */
 
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	int i, cont1 = 0, flag1 = 0;
+	char *conca;
+	int i, j, cont = 0, k = 0;
 
-	if (str == NULL || *str == '\0')
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; i < ac; i++)
 	{
-		if (str[i] != ' ' && flag1 == 0)
-		{
-			cont1++;
-			flag1 = 1;
-		}
-		else if (flag1 == 1 && str[i + 1] == ' ')
-			flag1 = 0;
+		for (j = 0; av[i][j] != '\0'; j++)
+			cont++;
 	}
-	if (cont1 == 0)
+
+	conca = malloc(cont * sizeof(char) + ac + 1);
+	if (!conca)
 		return (NULL);
 
-	return (spll(str, cont1));
-}
-
-/**
-  * spll - splits a string into words
-  * @str: a string with spaces
-  * @cont1: is a counter
-  * Return: an address
-  */
-
-char **spll(char *str, int cont1)
-{
-	int i, j, k, cont2, flag1 = 0;
-	char **splitt;
-
-	splitt = malloc((cont1 + 1) * sizeof(char *));
-	if (!splitt)
-		return (NULL);
-	for (i = 0, j = 0, flag1 = 0, cont2 = 0; i < cont1; i++, j++)
+	for (i = 0; i < ac; i++)
 	{
-		for ( ; str[j] != '\0'; j++)
-		{
-			if (str[j] != ' ')
-			{
-				cont2++;
-				flag1 = 1;
-			}
-			if (flag1 == 1 && (str[j + 1] == ' ' || str[j + 1] == '\0'))
-			{
-				flag1 = 0;
-				splitt[i] = malloc(cont2 * sizeof(char) + 1);
-				if (splitt[i] == NULL)
-				{
-					for (i--; i >= 0; i--)
-						free(splitt[i]);
-					free(splitt);
-					return (NULL);
-				}
-				for (k = 0; k < cont2; k++)
-					splitt[i][k] = str[(j - cont2 + 1) + k];
-				splitt[i][k] = '\0';
-				cont2 = 0;
-				break;
-			}
-		}
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			conca[k] = av[i][j];
+		conca[k] = '\n';
+		k++;
 	}
-	splitt[i] = '\0';
-	return (splitt);
+	conca[k] = '\0';
+	return (conca);
 }
